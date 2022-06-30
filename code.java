0c1fba6917194ee209b35b7c513e316dfdc6304b
kamesh20k <96664751+kamesh20k@users.noreply.github.com>
2022-06-30 09:18:55 +0530

    5th commit

--- code.java
+++ code.java
@@ -3,7 +3,7 @@ public class Main {
   public static void main(String[] args) {
     int rows = 7, k = 1;
 
-    for (int i = 1; i <= rows; ++i, k = 0) {
+    for (int i = 2; i <= rows; ++i, k = 0) {
       for (int space = 1; space <= rows - i; ++space) {
         System.out.print("  ");
       }
